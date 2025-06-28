e9e67bf5652fe898f40ecbecb620592d32f090ab
KIM BYOUNGGON <fharenheit@gmail.com>
2025-06-29 00:11:47 +0900

    리눅스에서는 파라미터를 ''으로 감싸야함.

--- toolkit/bin/ldap-test.sh
+++ toolkit/bin/ldap-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python3 ldap_validation.py --server ldaps://localhost:636 
-	--bind-dn DATALAKE\Administrator  --bind-password 11111 
-	--user-base-dn OU=Users,DC=datalake,DC=net \
-	--domain-name DATALAKE.NET \
-	--new-username test --new-username-password 11111
+python3 ldap_validation.py --server ldaps://localhost:636
+	--bind-dn 'DATALAKE\Administrator'  --bind-password '11111'
+	--user-base-dn 'OU=Users,DC=datalake,DC=net' \
+	--domain-name 'DATALAKE.NET' \
+	--new-username 'test' --new-username-password '11111'
