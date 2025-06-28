376425716415d9b97e6079285991cd356dad626d
Edward KIM <fharenheit@gmail.com>
2025-06-28 23:19:13 +0900

    Update gen-keytab.ps1

--- toolkit/bin/gen-keytab.ps1
+++ toolkit/bin/gen-keytab.ps1
@@ -1,13 +1,1 @@
-param(
-    [string]$principal,
-    [string]$mapuser
-)
-
-# 인자 체크
-if (-not $principal -or -not $mapuser) {
-    Write-Host "Usage: gen-keytab.ps1 -principal <VALUE> -mapuser <VALUE>"
-    Write-Host "Usage: gen-keytab.ps1 -principal cloudera@DATALAKE.NET -mapuser DATALAKE_PROD\cloudera"
-    exit 1
-}
-
-ktpass -out $principal.keytab -princ $principal -mapuser $mapuser -pass * -ptype KRB5_NT_PRINCIPAL -crypto all -mapop set
+ktpass -out cloudera.keytab -princ cloudera@DATALAKE.NET -mapuser DATALAKE\cloudera -pass * -ptype KRB5_NT_PRINCIPAL -crypto all -mapop set
