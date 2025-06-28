13e36a90c8eb57348da79a4c567b0fc7f799a7ce
Edward KIM <fharenheit@gmail.com>
2025-06-28 23:03:43 +0900

    Update gen-keytab.ps1

--- toolkit/bin/gen-keytab.ps1
+++ toolkit/bin/gen-keytab.ps1
@@ -6,8 +6,8 @@ param(
 
 # 인자 체크
 if (-not $principal -or -not $mapuser -or -not $password) {
-    Write-Host "사용법: gen-keytab.ps1 --principal <값> --mapuser <값> --password <값>"
-    Write-Host "사용법: gen-keytab.ps1 --principal cloudera@DATALAKE.NET --mapuser DATALAKE_PROD\cloudera --password @123qwe"
+    Write-Host "Usage: gen-keytab.ps1 --principal <VALUE> --mapuser <VALUE> --password <VALUE>"
+    Write-Host "Usage: gen-keytab.ps1 --principal cloudera@DATALAKE.NET --mapuser DATALAKE_PROD\cloudera --password @123qwe"
     exit 1
 }
 
